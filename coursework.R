26b8d6320ebe946294575720806762666153dbf6
NathanKhadaroo <nathanjames@hotmail.fr>
2020-05-20 18:29:30 +0100

    .

--- coursework.R
+++ coursework.R
@@ -1,4 +1,4 @@
-install.packages('fastDummies')
+install.packages("kableExtra")
 library(sjPlot)
 library(sjmisc)
 library(sjlabelled)
@@ -6,9 +6,11 @@ library(tidyverse)
 library(fastDummies)
 library(broom)
 library(lme4)
-
+library(magrittr)
+library(htmltable)
 
 #loading data
+setwd("~/Desktop/Multilevel-Modelling")
 
 mydataNAs <- read.table(file = "coursework.txt", sep = "," , header = TRUE)
 
@@ -21,6 +23,14 @@ mydata <- dummy_cols(mydata, select_columns = c('hhtenure', 'hiqual3'), remove_f
 mydata <- mydata %>% rename(rent_local_auth = hhtenure_2, rent_private = hhtenure_3,
                             school_qual = hiqual3_2, no_qual = hiqual3_3)
 colnames(mydata)
+
+#summary statistics
+a <- mydata %>%
+      group_by(region) %>% 
+      summarise(Number_of_households = n_distinct(hid),
+            Number_of_individuals = n(),
+            Mean_number_of_individuals_per_household = n()/n_distinct(hid)) %>%
+        
 
 #fiting models
 single_null <- lm(nhood_mistrust ~ 1, data = mydata)
@@ -38,4 +48,5 @@ summary(nullmodel_threelvl)
 #creating a table
 
 tab_model(single_null, twolevel_null_hid,twolevel_null_region,nullmodel_threelvl, 
-          dv.labels = c("Single level null", "Two levels (household)", "Two levels (region)", "Three levels"))
+          dv.labels = c("Single level null", "Two levels (household)", "Two levels (region)", "Three levels"),
+          show.se = TRUE, show.ci = FALSE, show.p = FALSE)
