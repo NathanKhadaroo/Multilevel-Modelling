01f79909959bccc8a941750b84d5b2a850c55ea5
NathanKhadaroo <nathanjames@hotmail.fr>
2020-05-21 21:41:31 +0100

    .

---
+++ caterpillar.R
@@ -0,0 +1,90 @@
+
+lapply(c("R2MLwiN", "arm","ggplot2","RColorBrewer","grid"),library, character.only=TRUE)
+
+# Set working directory and load data
+setwd("~/Desktop/Multilevel-Modelling")
+
+mydataNAs <- read.table(file = "coursework.txt", sep = "," , header = TRUE)
+
+mydata <- drop_na(mydataNAs)
+
+# Fit a linear varying intercept multilevel model with group level predictors of normexam ~ sex + standlrt + vrband + schgend + avslrt + schav + (1|school)
+fit <- lmer(nhood_mistrust ~ (1|hid), data = mydata, REML = FALSE)
+display(fit)
+
+# Display "Random"/"Varying" i.e. school effects
+ranef(fit)
+
+# Extract higher level residuals
+ranef <-ranef(fit)
+ranef.se<-se.ranef(fit)
+u0mn <-ranef$hid
+u0sd <-ranef.se$hid
+# Rank residuals
+
+u0rankmn <- rank(u0mn)
+u0hi <- u0mn + (1.96*u0sd)
+u0lo <- u0mn  - (1.96*u0sd)
+
+u0 <- ranef(fit, postVar = TRUE) 
+
+#You might get a warning message saying to use condVar instead postVar
+
+#Save the standard error of the school-level residuals in the object u0se
+
+u0se <- sqrt(attr(u0[[1]], "postVar")[1, , ]) 
+
+hid <- as.numeric(rownames(u0[[1]]))
+
+#This section of code creates a table of the school-level residuals and gives them a rank
+
+u0tab <- cbind(hid, u0[[1]], u0se)
+
+colnames(u0tab) <- c("hid","u0","u0se")
+
+u0tab <- u0tab[order(u0tab$u0), ]
+
+u0tab <- cbind(u0tab, c(1:dim(u0tab)[1]))
+
+colnames(u0tab)[4] <- "u0rank"
+
+u0tab <- u0tab[order(u0tab$hid), ]
+
+u0tab[1:10, ]
+
+# Combine into data.frame
+d <-data.frame(u0tab$u0rank, u0tab$u0, u0hi, u0lo) 
+
+# Set colors
+palette <- brewer.pal("Greys", n=9)
+color.background = palette[1]
+color.grid.major = palette[5]
+color.axis.text = palette[6]
+color.axis.title = palette[7]
+color.title = palette[9]
+
+# Plot
+
+k <- ggplot()+
+  geom_pointrange(data=u0tab,mapping=aes(x=u0tab$u0rank , y=u0tab$u0,ymin=d$X.Intercept..1,ymax=d$X.Intercept.), position="identity", size=0.1, color="dodgerblue4", alpha = 0.5)+
+  geom_point(data=u0tab,mapping=aes(x=u0tab$u0rank , y=u0tab$u0))+
+  theme_bw() +
+  theme(panel.background=element_rect(fill=color.background, color=color.background)) +
+  theme(plot.background=element_rect(fill=color.background, color=color.background)) +
+  theme(panel.border=element_blank(), axis.line=element_line(), axis.line.y=element_blank()) +  
+  theme(panel.grid.major=element_line(colour=color.background,size=.75)) +
+  theme(axis.ticks=element_blank()) +
+  theme(axis.line.x =element_line(colour="#535353", size=.75))+
+  theme(axis.line.y =element_line(colour="#535353", size=.75))+
+  theme(legend.position="none") +
+  ggtitle("Caterpillar plot of household level residuals")+ 
+  theme(plot.title=element_text(face="bold",hjust=-.08,vjust=2,colour="black",size=20)) +
+  xlab("Rank of residuals") +
+  ylab("Conditional modes of r.e. for hid:_cons") +
+  theme(axis.text.x=element_text(size=17,colour="#535353",face="bold")) +
+  theme(axis.text.y=element_text(size=17,colour="#535353",face="bold")) +
+  theme(axis.title.y=element_text(size=17,colour="#535353",face="bold",vjust=1.5)) +
+  theme(axis.title.x=element_text(size=17,colour="#535353",face="bold",vjust=-.5)) +
+  geom_hline(yintercept=0,size=1.2, alpha=0.8,colour="red", linetype="twodash")+
+  theme(plot.margin = unit(c(1, 1, .5, .7), "cm"))
+k
--- coursework.R
+++ coursework.R
@@ -1,4 +1,4 @@
-install.packages("kableExtra")
+
 library(sjPlot)
 library(sjmisc)
 library(sjlabelled)
@@ -7,7 +7,7 @@ library(fastDummies)
 library(broom)
 library(lme4)
 library(magrittr)
-library(htmltable)
+library(htmlTable)
 
 #loading data
 setwd("~/Desktop/Multilevel-Modelling")
@@ -24,12 +24,16 @@ mydata <- mydata %>% rename(rent_local_auth = hhtenure_2, rent_private = hhtenur
                             school_qual = hiqual3_2, no_qual = hiqual3_3)
 colnames(mydata)
 
+#centering age
+
+mydata <- mydata %>% mutate(age = age - 16)
+
 #summary statistics
 a <- mydata %>%
       group_by(region) %>% 
       summarise(Number_of_households = n_distinct(hid),
             Number_of_individuals = n(),
-            Mean_number_of_individuals_per_household = n()/n_distinct(hid)) %>%
+            Mean_number_of_individuals_per_household = n()/n_distinct(hid))
         
 
 #fiting models
@@ -50,3 +54,113 @@ summary(nullmodel_threelvl)
 tab_model(single_null, twolevel_null_hid,twolevel_null_region,nullmodel_threelvl, 
           dv.labels = c("Single level null", "Two levels (household)", "Two levels (region)", "Three levels"),
           show.se = TRUE, show.ci = FALSE, show.p = FALSE)
+
+#testing for improvement in fit 
+
+anova(nullmodel_threelvl, twolevel_null_hid)
+
+qchisq(0.95, 2)
+
+#cattepillar plot
+
+#Extract the school-level residuals and assign them to a new object u0
+
+u0 <- ranef(twolevel_null_hid, postVar = TRUE) 
+
+#You might get a warning message saying to use condVar instead postVar
+
+#Save the standard error of the school-level residuals in the object u0se
+
+u0se <- sqrt(attr(u0[[1]], "postVar")[1, , ]) 
+
+hid <- as.numeric(rownames(u0[[1]]))
+
+#This section of code creates a table of the school-level residuals and gives them a rank
+
+u0tab <- cbind(hid, u0[[1]], u0se)
+
+colnames(u0tab) <- c("hid","u0","u0se")
+
+u0tab <- u0tab[order(u0tab$u0), ]
+
+u0tab <- cbind(u0tab, c(1:dim(u0tab)[1]))
+
+colnames(u0tab)[4] <- "u0rank"
+
+u0tab <- u0tab[order(u0tab$schoolid), ]
+
+u0tab[1:10, ]
+
+#Now we can create the caterpillar plot - I've added a title for the plot in the first line
+
+plot(u0tab$u0rank, u0tab$u0, type = "n", xlab = "u_rank", 
+     ylab = "conditional modes of r.e. for hid:_cons", 
+     main = "Caterpillar plot of household level residuals")
+
+segments(u0tab$u0rank, u0tab$u0 - 1.96*u0tab$u0se, 
+         u0tab$u0rank, u0tab$u0 + 1.96*u0tab$u0se)
+
+points(u0tab$u0rank, u0tab$u0, col = "blue")
+
+abline(h = 0, col = "red")
+
+#calculating vpc
+#P11.2.3 Calculating coverage intervals, variance partition coefficients (VPCs) and intraclass correlation coefficients (ICCs)
+
+#Class-level variance 
+
+VarCorr(nullmodel_threelvl)$hid
+
+#School-level variance
+
+VarCorr(nullmodel_threelvl)$region
+
+#Save the residual variances as a dataframe
+
+vars_nullmodel_threelvl <- as.data.frame(VarCorr(nullmodel_threelvl))
+
+vars_nullmodel_threelvl
+
+#School-level VPC - this coincides with the school-level ICC for this model
+
+region_VPC <- vars_nullmodel_threelvl[2,4] / (vars_nullmodel_threelvl[1,4] + vars_nullmodel_threelvl[2,4] + vars_nullmodel_threelvl[3,4])
+
+region_VPC
+
+#Class-level VPC
+
+hid_VPC <- vars_nullmodel_threelvl[1,4] / (vars_nullmodel_threelvl[1,4] + vars_nullmodel_threelvl[2,4] + vars_nullmodel_threelvl[3,4])
+
+hid_VPC
+
+#Class-level ICC
+
+hid_ICC <- (vars_nullmodel_threelvl[1,4] + vars_nullmodel_threelvl[2,4]) / (vars_nullmodel_threelvl[1,4] + vars_nullmodel_threelvl[2,4] + vars_nullmodel_threelvl[3,4])
+
+hid_ICC
+
+#individual level
+
+pupil_VPC <- vars_nullmodel_threelvl[3,4] / (vars_nullmodel_threelvl[1,4] + vars_nullmodel_threelvl[2,4] + vars_nullmodel_threelvl[3,4])
+
+pupil_VPC
+
+
+#adding variables (intercept)
+
+model6 <- lmer(nhood_mistrust ~ age + sclfsato + urban + school_qual + no_qual + rent_local_auth + rent_private + (1|hid) + (1|region), data = mydata, REML = FALSE)
+
+tab_model(model6, 
+          dv.labels = c("Random intercept model"),
+          show.se = TRUE, show.ci = FALSE, show.p = FALSE, show.reflvl = TRUE)
+summary(model6)
+
+install.packages('texreg')
+library(texreg)
+htmlreg(list(model6), file = "coursework.R.doc", 
+        inline.css = FALSE, doctype = TRUE, html.tag = TRUE, 
+        head.tag = TRUE, body.tag = TRUE)
+
+
+
+
